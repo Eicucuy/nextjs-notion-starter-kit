34b00a86da3f392f3dbab642e833ba15b99a4cfd
Eicucuy <133969477+Eicucuy@users.noreply.github.com>
2023-05-19 11:53:40 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,27 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '5a42d4e9996641919752f0f4b9e40544',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '文心雕龙',
+  domain: 'https://spring-agate-bd5.notion.site/5a42d4e9996641919752f0f4b9e40544',
+  author: '倪颂博',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
-
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
